cad92150222440fbb732455020362e19a6d63172
Jacob Dolph <jacob.dolph702@gmail.com>
2020-01-23 23:23:57 -0700

    added screenshots of apps as the button images, and added links to the repos for each project

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,24 +1,17 @@
 $(window).on("load", function () {
     $(window).scroll(function () {
         var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-
         $(".fade").each(function () {
             var objectBottom = $(this).offset().top + $(this).outerHeight();
-
             if (objectBottom < windowBottom) {
                 if ($(this).css("opacity") == 0) { $(this).fadeTo(500, 1); }
             } else {
                 if ($(this).css("opacity") == 1) { $(this).fadeTo(500, 0); }
             };
-
-
         });
     }).scroll();
-
 });
-
 $("#fade-in").hide().fadeIn(3000);
-
 $(".button-1").click(function () {
     document.getElementById('home').scrollIntoView({
         block: "center",
